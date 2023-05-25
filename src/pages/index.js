7c0f66a3288afa8c3ddbe1813578ad6196c72448
MartaFagundez <martafagundez.dev@gmail.com>
2023-05-24 23:09:19 -0300

    Ajustes en la estructura de los directorios del proyecto

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Weather - Next App</title>
+        <meta name="description" content="Weather App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Hello</h1>
+      
+    </div>
+
+  )
+}
